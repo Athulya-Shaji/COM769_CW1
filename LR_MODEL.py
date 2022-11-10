229a84761f79cdcd10d7c47d5cfcba3ba50493ae
Athulya-Shaji <117929947+Athulya-Shaji@users.noreply.github.com>
2022-11-10 16:00:54 +0000

    Model1

---
+++ LR_MODEL.py
@@ -0,0 +1,27 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+data = np.loadtxt('physical.txt',delimiter='\t',skiprows=1)
+
+
+
+y = data[ : , [0]] # or data_from_file[ : , 0:1]
+x = data[:, 1:] # or data_from_file[ : , 1:2]
+# x
+print(x)
+
+lin = LinearRegression()
+
+model = lin.fit(x,y)
+
+coeff = lin.coef_
+
+print("The Coeff Weight is", coeff)
+
+intercept = lin.intercept_
+
+print("The Intercept Weights are", intercept)
+
+y_pred = model.predict(x)
+print(y_pred)
+print(y)
